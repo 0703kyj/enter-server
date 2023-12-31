8bcebdf4699d1f20f7d734fa57ca96d213d4fc42
0703kyj <kyj0703@konkuk.ac.kr>
2023-12-31 15:54:23 +0900

    init project

---
+++ src/test/java/com/enter/server/EnterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enter.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
